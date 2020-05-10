669c619cf42d0294feb7d3d35bdaeaaf31970033
Jijo Bose <bosejijo@gmail.com>
2020-05-10 09:56:29 +0530

    Card UI

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,76 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: NetworkImage(
-                  'https://avatars1.githubusercontent.com/u/13100108'
-                ),
+            child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: NetworkImage(
+                  'https://avatars1.githubusercontent.com/u/13100108'),
+            ),
+            Text(
+              'Jijo Bose',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontFamily: 'Pacifico',
               ),
-              Text(
-                'Jijo Bose',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontFamily: 'Pacifico',
-                ),
+            ),
+            Text(
+              'Flutter Developer',
+              style: TextStyle(
+                color: Colors.teal.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Source Sans Pro',
               ),
-            ],
-          )
-        ),
+            ),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      '+91 123 456 7890',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                )),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text(
+                      'contact@jijobose.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ))
+          ],
+        )),
       ),
     );
   }
