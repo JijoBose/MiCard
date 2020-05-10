fd977d7156a7f8b576cd91b9aed8291e48621135
Jijo Bose <bosejijo@gmail.com>
2020-05-10 13:31:57 +0530

    Indentation

--- lib/main.dart
+++ lib/main.dart
@@ -11,70 +11,64 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: NetworkImage(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage(
                   'https://avatars1.githubusercontent.com/u/13100108'),
-            ),
-            Text(
-              'Jijo Bose',
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-                fontFamily: 'Pacifico',
               ),
-            ),
-            Text(
-              'Flutter Developer',
-              style: TextStyle(
-                color: Colors.teal.shade100,
-                fontSize: 20.0,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Source Sans Pro',
-              ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.teal
-                ),
-                title: Text(
-                  '+91 123 456 789',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.teal.shade900,
-                    fontFamily: 'Source Sans Pro'
-                  )
+              Text(
+                'Jijo Bose',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
                 ),
               ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.email,
-                  color: Colors.teal,
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source Sans Pro',
                 ),
-                title: Text(
-                  'contact@jijobose.com',
-                  style: TextStyle(
-                    color: Colors.teal.shade900,
-                    fontFamily: 'Source Sans Pro',
-                    fontSize: 20.0,
-                  )
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text(
+                    '+91 123 456 789',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro'
+                    )
+                  ),
                 ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text('contact@jijobose.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    )
+                  ),
+                )
               )
-            )
-          ],
-        )),
+            ],
+          )
+        ),
       ),
     );
   }
